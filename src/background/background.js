6e4057dcccf255adae07ed606a43016ec238a865
Prathyush77 <prathyushakumalla@gmail.com>
2026-07-19 08:40:59 +0530

    feat: fetch and display HTTP status code for active tab

--- src/background/background.js
+++ src/background/background.js
@@ -1,1 +1,27 @@
-console.log("StatusPeek Background Service Worker Started 🚀");
+console.log("🚀 StatusPeek Background Service Worker Started");
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, async (tab) => {
+
+        console.log(tab);
+
+        if (
+            !tab.url ||
+            (
+                !tab.url.startsWith("http://") &&
+                !tab.url.startsWith("https://")
+            )
+        ) {
+            console.log("Unsupported URL");
+            return;
+        }
+
+        try {
+            const response = await fetch(tab.url);
+            console.log("HTTP Status:", response.status);
+        } catch (error) {
+            console.error("Fetch Failed:", error);
+        }
+
+    });
+});
